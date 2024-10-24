17884ca74074afa24b2cb9c0a21e46be35703e4d
Carlos Gomes <carlos_gomes1809@hotmail.com>
2024-10-23 23:21:45 -0300

    add main components

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -11,60 +11,62 @@ import {
 } from "lucide-react";
 // import useUpload, { StatusText } from "@/hooks/useUpload";
 import { useRouter } from "next/navigation";
+import useUpload, { StatusText, Status } from "@/hooks/useUpload";
 // import useSubscription from "@/hooks/useSubscription";
 // import { useToast } from "./ui/use-toast";
 
 function FileUploader() {
-  // const { progress, status, fileId, handleUpload } = useUpload();
+  const { progress, status, fileId, handleUpload } = useUpload() as {
+    progress: number | null;
+    status: StatusText;
+    fileId: string | null;
+    handleUpload: (file: File) => Promise<void>;
+  };
   // const { isOverFileLimit, filesLoading } = useSubscription();
   const router = useRouter();
   // const { toast } = useToast();
 
-  // useEffect(() => {
-  //   if (fileId) {
-  //     router.push(`/dashboard/files/${fileId}`);
-  //   }
-  // }, [fileId, router]);
+  useEffect(() => {
+    if (fileId) {
+      router.push(`/dashboard/files/${fileId}`);
+    }
+  }, [fileId, router]);
 
   const onDrop = useCallback(
     async (acceptedFiles: File[]) => {
       // Do something with the files
 
       const file = acceptedFiles[0];
-      // if (file) {
-      //   if (!isOverFileLimit && !filesLoading) {
-      //     await handleUpload(file);
-      //   } else {
-      //     toast({
-      //       variant: "destructive",
-      //       title: "Free Plan File Limit Reached",
-      //       description:
-      //         "You have reached the maximum number of files allowed for your account. Please upgrade to add more documents.",
-      //     });
-      //   }
-      // } else {
-      //   // do nothing...
-      //   // toast...
-      // }
+      if (file) {
+        // if (!isOverFileLimit && !filesLoading) {
+        await handleUpload(file);
+        // } else {
+        // toast({
+        //   variant: "destructive",
+        //   title: "Free Plan File Limit Reached",
+        //   description:
+        //     "You have reached the maximum number of files allowed for your account. Please upgrade to add more documents.",
+        // });
+      }
     },
     // [handleUpload, isOverFileLimit, filesLoading, toast]
-    []
+    [handleUpload]
   );
 
-  // const statusIcons: {
-  //   [key in StatusText]: JSX.Element;
-  // } = {
-  //   [StatusText.UPLOADING]: (
-  //     <RocketIcon className="h-20 w-20 text-indigo-600" />
-  //   ),
-  //   [StatusText.UPLOADED]: (
-  //     <CheckCircleIcon className="h-20 w-20 text-indigo-600" />
-  //   ),
-  //   [StatusText.SAVING]: <SaveIcon className="h-20 w-20 text-indigo-600" />,
-  //   [StatusText.GENERATING]: (
-  //     <HammerIcon className="h-20 w-20 text-indigo-600 animate-bounce" />
-  //   ),
-  // };
+  const statusIcons: {
+    [key in StatusText]: JSX.Element;
+  } = {
+    [StatusText.UPLOADING]: (
+      <RocketIcon className="h-20 w-20 text-indigo-600" />
+    ),
+    [StatusText.UPLOADED]: (
+      <CheckCircleIcon className="h-20 w-20 text-indigo-600" />
+    ),
+    [StatusText.SAVING]: <SaveIcon className="h-20 w-20 text-indigo-600" />,
+    [StatusText.GENERATING]: (
+      <HammerIcon className="h-20 w-20 text-indigo-600 animate-bounce" />
+    ),
+  };
 
   const { getRootProps, getInputProps, isDragActive, isFocused, isDragAccept } =
     useDropzone({
@@ -75,14 +77,14 @@ function FileUploader() {
       },
     });
 
-  // const uploadInProgress = progress != null && progress >= 0 && progress <= 100;
+  const uploadInProgress = progress != null && progress >= 0 && progress <= 100;
 
   return (
     <div className="flex flex-col gap-4 items-center max-w-7xl mx-auto">
       {/* Loading... tomorrow! */}
-      {/* {uploadInProgress && ( */}
+      {uploadInProgress && (
         <div className="flex flex-col justify-center items-center gap-5">
-          {/* <div
+          <div
             className={`radial-progress bg-indigo-300 text-white border-indigo-600 border-4 ${
               progress === 100 && "hidden"
             }`}
@@ -95,17 +97,17 @@ function FileUploader() {
               } as React.CSSProperties
             }
           >
-            {/* {progress} %
-          </div> */}
+            {progress} %
+          </div>
 
           {/* Render Status Icon */}
-          {/* {statusIcons[status!]} */}
+          {statusIcons[status]}
 
-          <p className="text-indigo-600 animate-pulse">{status}</p>
+          <p className="text-indigo-600 animate-pulse">{status.toString()}</p>
         </div>
-      {/* )} */}
+      )}
 
-      {/* {!uploadInProgress && ( */}
+      {!uploadInProgress && (
         <div
           {...getRootProps()}
           className={`p-10 border-2 border-dashed mt-10 w-[90%]  border-indigo-600 text-indigo-600 rounded-lg h-96 flex items-center justify-center ${
@@ -128,8 +130,9 @@ function FileUploader() {
             )}
           </div>
         </div>
-      {/* )} */}
+      )}
     </div>
   );
 }
+
 export default FileUploader;
--- components/PlaceholderDocument.tsx
+++ components/PlaceholderDocument.tsx
@@ -26,14 +26,14 @@ function PlaceholderDocument() {
       className="flex flex-col items-center w-64 h-80 rounded-xl bg-gray-200 drop-shadow-md text-gray-400"
     >
       {isOverFileLimit ? (
-        <FrownIcon className="h-16 w-16" />
+        <FrownIcon className="!h-16 !w-16" />
       ) : (
-        <PlusCircleIcon className="h-16 w-16" />
+        <PlusCircleIcon className="!h-16 !w-16" />
       )}
 
-      <p className="font-semibold">
+      <span className="font-semibold">
         {isOverFileLimit ? "Upgrade to add more documents" : "Add a document"}
-      </p>
+      </span>
     </Button>
   );
 }
